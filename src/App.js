23ed0f26efffd56860acafa1e23ce2dfd8662fba
ocmino <ocmino@gmail.com>
2023-03-01 11:58:06 +0100

    dgdfgd

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ import { XR, ARButton, Controllers } from "@react-three/xr";
 import Experience from "./components/Experience";
 import Configurator from "./components/Configurator";
 import { CustomizationProvider } from "./context/Customization";
-import XRPool from "./Xrpool";
+import XRModel from "./Xrmodel";
+import { MeshReflectorMaterial } from "@react-three/drei";
 
 function App() {
   return (
     <div className="App">
       <CustomizationProvider>
-        <ARButton />
+        <ARButton sessionInit={{ requiredFeatures: ["hit-test"] }}/>
         <Canvas>
           <XR referenceSpace="local">
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <Controllers />
             <Experience />
-            <XRPool
-              scale={[0.5, 0.5, 0.5]}
-              position={[0, 0, 0]}
-              rotation={[0, 0, 0]}
-            />
+            <XRModel />
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+              <circleGeometry attach="geometry" args={[100, 100]} />
+              <MeshReflectorMaterial attach="material" color="lightgrey" />
+            </mesh>
           </XR>
         </Canvas>
         <Configurator />
--- src/Xrmodel.jsx
+++ src/Xrmodel.jsx
@@ -10,7 +10,7 @@ import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
 
 const XRModel = (props) => {
-  const { nodes } = useGLTF("/models/xrmodel.gltf");
+  const { nodes } = useGLTF("/models/xrmodel2.gltf");
   const { shape, color } = useCustomization();
 
   const GlassTexture_1 = useTexture({
@@ -37,13 +37,13 @@ const XRModel = (props) => {
     aoMap: '/textures/Glass_2/Blue_Ice_001_OCC.jpg',
   })
 
-  GlassTexture_2.map.repeat.set(4, 4)
+  GlassTexture_2.map.repeat.set(8, 8)
   GlassTexture_2.map.wrapS = GlassTexture_2.map.wrapT = THREE.RepeatWrapping
-  GlassTexture_2.normalMap.repeat.set(4, 4)
+  GlassTexture_2.normalMap.repeat.set(8, 8)
   GlassTexture_2.normalMap.wrapS = GlassTexture_2.normalMap.wrapT = THREE.RepeatWrapping
-  GlassTexture_2.roughnessMap.repeat.set(4, 4)
+  GlassTexture_2.roughnessMap.repeat.set(8, 8)
   GlassTexture_2.roughnessMap.wrapS = GlassTexture_2.roughnessMap.wrapT = THREE.RepeatWrapping
-  GlassTexture_2.aoMap.repeat.set(4, 4)
+  GlassTexture_2.aoMap.repeat.set(8, 8)
   GlassTexture_2.aoMap.wrapS = GlassTexture_2.aoMap.wrapT = THREE.RepeatWrapping
   GlassTexture_2.side = THREE.DoubleSide
 
@@ -61,4 +61,4 @@ const XRModel = (props) => {
 
 export default XRModel;
 
-useGLTF.preload("/models/xrmodel.gltf");
+useGLTF.preload("/models/xrmodel2.gltf");
