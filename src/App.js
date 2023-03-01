dae6402e4999704b7666fdae1d0b1aeb1368cf20
ocmino <ocmino@gmail.com>
2023-03-01 12:05:33 +0100

    sdfsdfsf

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <CustomizationProvider>
-        <ARButton sessionInit={{ requiredFeatures: ["hit-test"] }}/>
+        <ARButton/>
         <Canvas>
           <XR referenceSpace="local">
             <ambientLight />
